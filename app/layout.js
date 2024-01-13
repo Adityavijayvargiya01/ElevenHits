580cbd6ed1474a845ae381c73b16fc104e294633
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-01-13 20:10:59 +0530

    Update title in metadata

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'ElvenHits',
+  title: 'ElevenHits',
   description: 'ElvenHits is a brand that sells cosmetics and beauty products.',
 }
 
