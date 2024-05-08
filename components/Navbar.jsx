341407190544fcf5f340ab76187abe968b200be6
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-05-08 22:03:22 +0530

    Commited from vim :)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,20 +13,20 @@ import { Bars3Icon , DevicePhoneMobileIcon , ShoppingCartIcon} from '@heroicons/
 
 export default function Navbar() {
   return (
-    <main className=' sticky'>
-    <div className='lg:flex p-10 justify-between hidden items-center'>
+    <main className='sticky'>
+    <div className='lg:flex p-5 justify-between hidden items-center'>
     <div className="font-extrabold text-5xl">
       ElevenHits
     </div>
     <div>
-    <ul className='flex text-lg font-medium gap-10 px-10 py-2 rounded-2xl bg-accent-foreground text-accent'>
+    <ul className='flex text-lg font-medium gap-12 px-7 py-2 rounded-full bg-primary text-accent'>
       <a href="/">Home</a>
       <a href="/Products">Products</a>
       <a href="https://github.com/Adityavijayvargiya01/ElevenHits">About</a>
       <a href="https://github.com/Adityavijayvargiya01/ElevenHits">Contact</a>
     </ul>
     </div>
-    <Button className='bg-accent-foreground text-accent px-4 py-2 rounded-xl font-medium'>Cart <ShoppingCartIcon className='h-6 w-6 m-1'/></Button>
+    <Button className='bg-accent-foreground text-accent px-4 py-2 rounded-full font-medium'>Cart <ShoppingCartIcon className='h-6 w-6 m-1'/></Button>
     </div>
     <div className='lg:hidden p-8 flex justify-between'>
     <div className=" text-4xl font-extrabold">
